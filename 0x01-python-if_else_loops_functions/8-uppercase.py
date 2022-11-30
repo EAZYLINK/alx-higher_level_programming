88443adba11040412193898f54b6f5e6de22ef97
Ezekiel Nwuguru <ezekiel.nwuguru@eng.uniben.edu>
2022-11-30 05:34:04 -0800

    new commit

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,6 +2,8 @@
 def uppercase(str):
     result = ''
     for char in str:
-        if ord(char) >= 65:
+        if ord(char) <= 90:
+            result += chr(ord(char))
+        elif ord(char) >= 97:
             result += chr(ord(char)-32)
-    return result
+    print("{:s}".format(result))
