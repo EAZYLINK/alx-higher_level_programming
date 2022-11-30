5281a93f4daf3cf9de8bb03d73a01914f344bbd7
Ezekiel Nwuguru <ezekiel.nwuguru@eng.uniben.edu>
2022-11-30 09:56:28 -0800

    new commit

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * insert_number - inserts a node to a list
+ * @head: node head
+ * @number: number of nodes
+ * Return: listint_t
+ */
+
+listint_t *insert_number(listint-t **head, int number)
+{
+listint_t *new;
+listint_t *current;
+current = *head;
+new = malloc(sizeof(listint_t));
+if (new == NULL)
+return (NULL);
+new->n = number;
+new->next = NULL;
+if (*head == NULL)
+*head = new;
+else
+{
+while (current->next != NULL)
+current = current->next;
+current->next = new;
+}
+return (new);
+}
