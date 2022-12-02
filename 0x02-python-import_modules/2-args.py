a60b3bbf4d4d2db1396448e67e199c526564ba95
Ezekiel nwuguru <eazy4life2017@gmail.com>
2022-12-02 05:00:53 +0100

    Create 2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ = "__main__":
+  import sys
+  count = len(sys.argv)-1
+  if count == 0:
+    print(f"{count} arguments.")
+  elif count == 1:
+    print(f"{count} arguments:")
+  else:
+    print(f"{count}: arguments:")
+  for i in range(count):
+    print(f"{i+1): {sys.argv[i+1]}")
