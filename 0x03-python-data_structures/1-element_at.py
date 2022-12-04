76a4f495f7c9010e046eecafeecc47288a05d20c
Ezekiel Nwuguru <ezekiel.nwuguru@eng.uniben.edu>
2022-12-03 22:11:13 -0800

    new commit

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,18 +1,10 @@
 def element_at(my_list, idx):
-  if idx < 0:
-<<<<<<< HEAD
-    str = "None"
-    return str
-  if idx > len(my_list):
-    str = "None"
-    return str
-  else:
-    str = my_list[idx]
-    return str
-=======
-    reurn None
-  elif idx > len(my_list):
-    return None
-  else:
-    return my_list[idx]
->>>>>>> e28fcab4f9aa08a937af53eec97b8f38c79b3d9d
+    if idx < 0:
+        str = "None"
+        return str
+    if idx > len(my_list):
+        str = "None"
+        return str
+    else:
+        str = my_list[idx]
+        return str
