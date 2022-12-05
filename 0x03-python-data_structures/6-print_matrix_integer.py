182466efcccfda943833d14357258f74d0fa0d22
Ezekiel Nwuguru <ezekiel.nwuguru@eng.uniben.edu>
2022-12-05 15:15:57 -0800

    new commit

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,6 @@ def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for j in i:
             if i.index(j) == len(i) - 1:
-                print("{}".format(j))
+                print("{:d}".format(j))
                 continue
-            print("{} ".format(j), end="")
+            print("{:d} ".format(j), end="")
