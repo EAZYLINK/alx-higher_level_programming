6ac9b754c2c3b8c3e6428d7c9e110ee378b0225e
Ezekiel nwuguru <eazy4life2017@gmail.com>
2023-03-15 01:07:47 +0100

    Update 4-rectangle.js

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,10 +1,7 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
   constructor (w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    }
+    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
   }
 
   print () {
@@ -14,12 +11,10 @@ module.exports = class Rectangle {
   }
 
   rotate () {
-    this.width = this.height;
-    this.height = this.width;
+    [this.width, this.height] = [this.height, this.width];
   }
 
   double () {
-    this.width = this.width * 2;
-    this.height = this.height * 2;
+    [this.width, this.height] = [this.width * 2, this.height * 2];
   }
 };
