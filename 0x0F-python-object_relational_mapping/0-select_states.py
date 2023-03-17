a08de1d3900ed6719615ea863eff6f06e74dec04
Ezekiel Nwuguru <ezekiel.nwuguru@eng.uniben.edu>
2023-03-17 01:58:44 -0700

    new commit

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,14 +8,4 @@ if __name__ = "__main__":
     db = MYSQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
     c.execute("SELECT * FROM `states`")
-    [print(state) from state in c.fetchall()]#!/usr/bin/python3
-    """script that lists all states from the database hbtn_0e_0_usa"""
-
-    import sys
-    import MYSQLdb
-
-    if __name__ = "__main__":
-        db = MYSQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-        c = db.cursor()
-        c.execute("SELECT * FROM `states`")
-        [print(state) from state in c.fetchall()]
+    [print(state) from state in c.fetchall()]
