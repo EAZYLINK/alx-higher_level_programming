33565d8c77ea197e02981832ce6258da4f24af26
Ezekiel nwuguru <eazy4life2017@gmail.com>
2023-03-20 02:40:14 +0100

    Update 100-relationship_states_cities.py

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,4 +1,4 @@
-i!/usr/bin/python3
+#!/usr/bin/python3
 """
 creates the State "California" with the City "San Francisco" from the
 database hbtn_0e_100_usa: (100-relationship_states_cities.py)
@@ -13,7 +13,8 @@ from relationship_state import State
 from relationship_city import Base, City
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
     session_maker = sessionmaker(bind=engine)
     session = session_maker()
